071bfebe243040f0d3a1820337205c04c8d0eb57
c0laaaa <3357904035@qq.com>
2021-12-18 01:34:21 -0800

    修改了getop函数，让它可以识别负数

--- SourceFile/getop.c
+++ SourceFile/getop.c
@@ -11,13 +11,16 @@ int getop(char s[]){
     s[1] = '\0';
 
     // 判断这个第一个不是空白符号的字符是否是数字字符或数字字符的一部分
-    if(!isdigit(c) && c != '.')
+    if(!isdigit(c) && c != '.' && c !='-')
         return c;
-
-    // 如果这个第一个不是空白符号的字符是数字字符或数字字符的一部分
-    // 那么就从这个字符后的第二个字符开始将数字字符写入到s中
-    // 第一个字符已经写入到s中了
     i = 0;
+
+    // 判断'-'号是否是程序的一部分
+    if(!isdigit(s[++i] = c = getch()) && c != '.'){
+        ungetch(c);
+        return '-';
+    }
+
 
     if(isdigit(c)){
         while(isdigit(s[++i] = c = getch()))
